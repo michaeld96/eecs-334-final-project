a8b3a7b9d19ddb14dcc311e105d6d6c3a3a0e01d
Michael Dick <michaeldick000@gmail.com>
2023-03-19 01:32:49 -0400

    on chapter 8

--- src/sphere.hpp
+++ src/sphere.hpp
@@ -15,10 +15,6 @@ class sphere : public hittable
     sphere(point3 cen, double r) : center(cen), radius(r) {};
 
     virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
-
-    public:
-    point3 center;
-    double radius;
 };
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const 
@@ -47,7 +43,10 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     }
     rec.t = root;
     rec.p = r.at(rec.t);
-    rec.normal = (rec.p - center) / radius;
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
+
+    return true;
 }
 
 #endif
