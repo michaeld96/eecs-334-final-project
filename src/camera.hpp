9c648fe7cbba2846c0b67886d6dbcb848c202c30
Michael Dick <michaeldick000@gmail.com>
2023-04-01 17:48:45 -0400

    added an image

--- src/camera.hpp
+++ src/camera.hpp
@@ -3,52 +3,52 @@
 
 #include "common.hpp"
 
-class camera
-{
-    public: 
-    point3 origin;
-    point3 lower_left_corner;
-    vec3 horizontal;
-    vec3 vertical;
-    vec3 u, v, w;
-    double lens_radius;
-
-
+class camera {
     public:
-    camera(
-        point3 look_from, 
-        point3 look_at, 
-        vec3 v_up, 
-        double vertical_field_of_view,
-        double aspect_ratio,
-        double aperture,
-        double focus_distance
-        )
-    {
-        auto theta = degrees_to_radians(vertical_field_of_view);
-        auto height = tan(theta / 2);
-        auto view_port_height = 2.0 * height;
-        auto view_port_width = aspect_ratio * view_port_height;
-        
-        w = unit_vector(look_from - look_at);
-        u = unit_vector(cross(v_up, w));
-        v = cross(w,u);
+        camera(
+            point3 lookfrom,
+            point3 lookat,
+            vec3   vup,
+            double vfov, // vertical field-of-view in degrees
+            double aspect_ratio,
+            double aperture,
+            double focus_dist
+        ) {
+            auto theta = degrees_to_radians(vfov);
+            auto h = tan(theta/2);
+            auto viewport_height = 2.0 * h;
+            auto viewport_width = aspect_ratio * viewport_height;
 
-        origin = look_from;
-        horizontal = focus_distance * view_port_width * u;
-        vertical = focus_distance * view_port_height * v;
-        lower_left_corner = origin - horizontal/2 - vertical/2 - focus_distance * w;
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
 
-        lens_radius = aperture / 2;
-    }
+            origin = lookfrom;
+            horizontal = focus_dist * viewport_width * u;
+            vertical = focus_dist * viewport_height * v;
+            lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w;
 
-    ray get_ray(double s, double t) const
-    {            
-        vec3 rd = lens_radius * random_in_unit_disk();
-        vec3 offset = u * rd.x() + v * rd.y();
+            lens_radius = aperture / 2;
+        }
 
-        return ray( origin + offset, lower_left_corner + s*horizontal + t*vertical - origin - offset);
-    }
+
+        ray get_ray(double s, double t) const {
+            vec3 rd = lens_radius * random_in_unit_disk();
+            vec3 offset = u * rd.x() + v * rd.y();
+
+            return ray(
+                origin + offset,
+                lower_left_corner + s*horizontal + t*vertical - origin - offset
+            );
+        }
+
+    private:
+        point3 origin;
+        point3 lower_left_corner;
+        vec3 horizontal;
+        vec3 vertical;
+        vec3 u, v, w;
+        double lens_radius;
 };
 
 #endif  // CAMERA_H
